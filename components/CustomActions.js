d09f32d446df77598fd0a782f279039206df2435
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-04-07 17:21:55 +0200

    created onActionPress() method with action options

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,7 +1,46 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+// import to make ActionSheet avaliable to fetch
+import { useActionSheet } from '@expo/react-native-action-sheet';
+
 const CustomActions = ({ wrapperStyle, iconTextStyle, color }) => {
-  const onActionPress = () => {};
+  //setting variable actionSheet value as useActionSheet() method, which returns a reference to Gifted Chat’s ActionSheet
+  const actionSheet = useActionSheet();
+
+  // onActionsPress renders additional actions for user
+  const onActionPress = () => {
+    // variable defines array of avaliable options
+    const options = [
+      'Choose image from library',
+      'Take picture',
+      'Send location',
+      'Cencel',
+    ];
+
+    // variable defines index of option 'Cancel'
+    const cancelButtonIndex = options.length - 1;
+
+    actionSheet.showActionSheetWithOptions(
+      { options, cancelButtonIndex },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            pickImage();
+            console.log('User wants to pick an image');
+            return;
+          case 1:
+            takePhoto();
+            console.log('User wants to take a photo');
+            return;
+          case 2:
+            getLocation();
+            console.log('User wants to share location');
+          default:
+        }
+      }
+    );
+  };
+
   return (
     <TouchableOpacity style={styles.container} onPress={onActionPress}>
       <View
